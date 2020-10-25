f3113cd50fa2b74994f0eefba4f8c84427ea474e
Dylan Tyrie-Dron <dman9887@gmail.com>
2020-10-25 13:38:45 +0000

    got github repos from github api

--- portfolio-site/src/pages/PortfolioPage.js
+++ portfolio-site/src/pages/PortfolioPage.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Content from '../components/Content';
+import Hero from '../components/Hero';
 
 function PortfolioPage(props){
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/dtyriedron/repos')
+        .then(res => res.json())
+        .then(data => {
+            setData(data);     
+        });
+    }, []);
+
+    
+    const setData = (
+        array,
+    ) => {
+        console.log(array)
+        setName(array[3].name);
+        setDescription(array[3].description);
+    };
+
+
     return(
-        <p>Portfolio Works!</p>
+        <div>
+            <Hero title={name}/>
+            <Content>
+                <p>
+                    {description}
+                </p>
+            </Content>
+        </div>
     );
 }
 export default PortfolioPage;
