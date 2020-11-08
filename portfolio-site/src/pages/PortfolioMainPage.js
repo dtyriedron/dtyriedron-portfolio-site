cb98ef5ee91709bc0737af988f19026ef0d0f50b
Dylan <dman9887@gmail.com>
2020-11-08 17:12:51 +0000

    got cards to work although now having issues setting a parallax bg

--- portfolio-site/src/pages/PortfolioMainPage.js
+++ portfolio-site/src/pages/PortfolioMainPage.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Hero from '../components/Hero';
-import { Card, Button} from 'react-bootstrap';
+import { Row, Col, Card, Button, CardDeck, Container} from 'react-bootstrap';
 import Content from '../components/Content';
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
 
 function PortfolioMainPage(props){
+    const [name, setName] = useState('');
     const [repos, setRepos] = useState('');
+    const bgImage = "../res/tri.jpg";
+
 
     useEffect(() => {
         fetch('https://api.github.com/users/dtyriedron/repos')
@@ -18,41 +22,53 @@ function PortfolioMainPage(props){
     const setCardData = (
         array,
     ) => {
+        setName(props.title);
         var arr = array;
         console.log(arr);
         setRepos(arr);
     };
 
+    
+
     return(
         <div>
-            <Hero title={props.title}>
-                <Content>
-                
-                    {
-                        Object.entries(repos).map(([int, repo], i) => {
-                            console.log(repo.name);
-                                return (          
-                                    <div key={i}>
-                                        <p>
-                                            repo name:{repo.name}
-                                            repo description:{repo.description}
-                                        </p>
-                                        {/* <Card style={{ minWidth: '450', margin: 'auto' }} key={i}>
-                                            <Card.Body key={i}>
-                                                <Card.Title key={i}>{repo.name}</Card.Title>
+            {/* <Hero title={name}> */}
+                {/* <Content> */}
+                    { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }                
+                    <Container fluid={true} style={{padding: '1rem'}}>
+                        {/* <Parallax ref={ref => (this.parallax = ref)} pages={1}>
+                            <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: {bgImage}, backgroundSize: 'cover' }} /> */}
+                            <Row md={12} className="justify-content-center">
+                            {
+                                Object.entries(repos).map(([int, repo], i) => {
+                                    return (         
+                                        // <Col> 
+                                        <Card border="primary" key={i} style={{width: '15rem', margin: '1rem'}}>
+                                            <Card.Header key={i}>{repo.name}
+                                            </Card.Header>
+                                            <Card.Body key={i}>                                            
                                                 <Card.Text key={i}>
                                                     {repo.description}
-                                                </Card.Text>
-                                                <Button key={i} variant="primary">Go somewhere</Button>
+                                                </Card.Text>                                            
                                             </Card.Body>
-                                        </Card> */}
-                                    </div>                                           
-                                )
-                        })
-                    }
+                                            <Card.Footer>
+                                                <Button key={i} variant="primary">GitHub</Button>
+                                            </Card.Footer>
+                                        </Card>
+                                        // </Col>
+                                    )
+                                })
+                            }
+
+                            </Row>
+                        {/* </Parallax> */}
+                    </Container>
+                {/* </Content> */}
                 
-                </Content>
-            </Hero>
+                    
+                
+                
+            {/* </Hero> */}
         </div>
     );
 }
