0e89105e2538eb094bfaaf5449d1ecffff739ce4
Dylan <dman9887@gmail.com>
2020-11-14 18:26:21 +0000

    maybe links fixed??

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -58,12 +58,12 @@ class App extends React.Component {
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/about">About</Link>
-                <Link className="nav-link" to="/PortfolioMainPage">Portfolio</Link>
-                <Link className="nav-link" to="/achievements">Achievements</Link>
-                <Link className="nav-link" to="/hobbies">Hobbies</Link>
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/dtyriedron-portfolio-site/">Home</Link>
+                <Link className="nav-link" to="/dtyriedron-portfolio-site/about">About</Link>
+                <Link className="nav-link" to="/dtyriedron-portfolio-site/PortfolioMainPage">Portfolio</Link>
+                <Link className="nav-link" to="/dtyriedron-portfolio-site/achievements">Achievements</Link>
+                <Link className="nav-link" to="/dtyriedron-portfolio-site/hobbies">Hobbies</Link>
+                <Link className="nav-link" to="/dtyriedron-portfolio-site/contact">Contact</Link>
 
               </Nav>
             </Navbar.Collapse>
