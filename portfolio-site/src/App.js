ee845a974cb325b8d768b3f8248b009d3ae73509
Dylan <dman9887@gmail.com>
2020-11-14 18:22:51 +0000

    fixing link issues...

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -71,7 +71,7 @@ class App extends React.Component {
 
           </Navbar>
 
-          <Route path="/" exact render={()=> <Home title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />}/>
+          <Route path="/" render={()=> <Home title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />}/>
           <Route path="/about" render={()=> <About title={this.state.about.title}/>}/>
           <Route path="/PortfolioMainPage" render={()=> <PortfolioMain title={this.state.portfolioMainPage.title}/>}/>
           <Route path="/achievements" render={()=> <Achievements title={this.state.achievements.title}/>}/>
