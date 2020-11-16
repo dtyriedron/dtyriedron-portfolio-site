e79fd71ea0d6d59b978399e3a265c5a2b9f70aca
Dylan Tyrie-Dron <dman9887@gmail.com>
2020-11-16 10:22:29 +0000

    Updates

---
+++ precache-manifest.5183e5e645061527aa43571508a4fa92.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1f66bcf71f4e2edda02525558908f5",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "d4786c94434d64f2f15c",
+    "url": "/portfolio-site/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "49e961ff0160c77f97b4",
+    "url": "/portfolio-site/static/css/main.b1726a8f.chunk.css"
+  },
+  {
+    "revision": "d4786c94434d64f2f15c",
+    "url": "/portfolio-site/static/js/2.e5fb6fb7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-site/static/js/2.e5fb6fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e961ff0160c77f97b4",
+    "url": "/portfolio-site/static/js/main.c0b3e1a6.chunk.js"
+  },
+  {
+    "revision": "a2a773adb23ff47de08a",
+    "url": "/portfolio-site/static/js/runtime-main.2d95e7a6.js"
+  },
+  {
+    "revision": "8f6ed3012123a3f31b5f5b0ef5377164",
+    "url": "/portfolio-site/static/media/DylanGH.8f6ed301.png"
+  },
+  {
+    "revision": "0ec24b3087b02daceeac21de7c043512",
+    "url": "/portfolio-site/static/media/DylanStrava.0ec24b30.png"
+  },
+  {
+    "revision": "54170236db11e0fc8448fc6e75d38965",
+    "url": "/portfolio-site/static/media/android.54170236.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dtyriedron-portfolio-site/precache-manifest.c2ba0d3a50f5a93c31381ffd863f6ddd.js"
+  "/portfolio-site/precache-manifest.5183e5e645061527aa43571508a4fa92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dtyriedron-portfolio-site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-site/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
